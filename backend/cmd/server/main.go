13cc62e6f3630b40eec68c764824d37fb262d522
go8089 <gopaljha8409@gmail.com>
2026-07-24 17:52:39 +0000

    config-health-router-main

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Go8089/page-pulse/internal/app"
+	"github.com/Go8089/page-pulse/internal/router"
+)
+
+func main() {
+	application, err := app.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := router.New(application)
+
+	application.Logger.Info("Starting server",
+		// We'll replace this with structured fields in the next sprint
+	)
+
+	if err := r.Run(":" + application.Config.ServerPort); err != nil {
+		log.Fatal(err)
+	}
+}
